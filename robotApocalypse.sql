8ef42eb192fb9518ccc326abf74cb2f11f6422ce
Sinazo Ndovela <152862396+sinazondove@users.noreply.github.com>
2024-02-16 15:17:28 +0200

    created all the rest APIs, to add survivors, to update survivor location, and to update the status if reported with contamination for more than 3 times, also updated robotApocalyse.slq to have the command sql script to create the table that will have survivor resources.

--- robotApocalypse.sql
+++ robotApocalypse.sql
@@ -8,3 +8,13 @@ CREATE TABLE Survivor_Status (
     times_reported_infected INT DEFAULT 0,
     FOREIGN KEY (survivor_id) REFERENCES Survivors(survivor_id)
 );
+-- Table to store resources possessed by survivors
+CREATE TABLE Survivor_Resources (
+    resource_id INT PRIMARY KEY AUTO_INCREMENT,
+    survivor_id INT,
+    water INT DEFAULT 0,
+    food INT DEFAULT 0,
+    medication INT DEFAULT 0,
+    ammunition INT DEFAULT 0,
+    FOREIGN KEY (survivor_id) REFERENCES Survivors(survivor_id)
+);
